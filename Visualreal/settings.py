70edc24744bbfd17901d8048bd6841186f6e176d
BeholdenArt <priyanshub5645@gmail.com>
2022-09-14 16:45:49 +0530

    v2.1

--- Visualreal/settings.py
+++ Visualreal/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os 
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    # 'django.contrib.staticfiles',
 
     'Company',
     'Dealer',
@@ -145,3 +146,5 @@ AUTH_USER_MODEL = 'Users.User'
 
 LOGOUT_REDIRECT_URL = 'login'
 
+
+django_heroku.settings(locals())
